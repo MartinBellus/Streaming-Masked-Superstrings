cbc366df89f0bf0a1007463d67bd88e6869ed114
MartinBellus <martin.bellus.4@gmail.com>
2025-03-24 17:18:08 +0100

    Init

---
+++ cmake/StandardProjectSettings.cmake
@@ -0,0 +1,15 @@
+# Set Release as the default build type
+set(default_build_type "Release")
+
+# Set the build type to Release if not already set
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+    set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+        STRING "Choose the type of build (Debug, Release)" FORCE)
+endif()
+
+set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+set(CMAKE_CXX_FLAGS_DEBUG "-g -O0 -Wall -Wextra")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
