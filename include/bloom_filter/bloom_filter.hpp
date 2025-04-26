6a91ec12b9821a850d24f88280ad2bd15b75c34c
MartinBellus <martin.bellus.4@gmail.com>
2025-04-26 10:46:53 +0200

    Decide bloom filter size at runtime

--- include/bloom_filter/bloom_filter.hpp
+++ include/bloom_filter/bloom_filter.hpp
@@ -3,59 +3,63 @@
 
 #include "hash_family.hpp"
 #include "helper/bitset.hpp"
+#include "math/modular.hpp"
 #include <string>
 
-template <std::size_t S, HashFamily H>
+template <HashFamily H>
 class BloomFilter {
   public:
-    BloomFilter(std::size_t nhashes) : hash_family(nhashes), data(S) {}
+    BloomFilter(std::size_t size, std::size_t nhashes)
+        : _size(size), hash_family(nhashes), data(size) {}
     void insert(const std::string &key) {
         for (auto &&h : hash_family.hash(key)) {
-            data.set((h % S));
+            data.set(_size.reduce(h));
         }
     }
     bool contains(const std::string &key) const {
         bool contains = true;
         for (auto &&h : hash_family.hash(key)) {
-            contains &= data.test(h % S);
+            contains &= data.test(_size.reduce(h));
         }
         return contains;
     }
 
   private:
     DynamicBitset data;
+    Modulus _size;
     mutable H hash_family;
 };
 
-template <std::size_t S, RollingHashFamily H>
+template <RollingHashFamily H>
 class RollingBloomFilter {
   public:
-    RollingBloomFilter(std::size_t nhashes, std::size_t k)
-        : hash_family(nhashes, k), data(S) {}
+    RollingBloomFilter(std::size_t size, std::size_t nhashes, std::size_t k)
+        : _size(size), hash_family(nhashes, k), data(size) {}
     void init(const std::string &key) { hash_family.init(key); }
     void roll(char c) { hash_family.roll(c); }
     void insert_this() {
         for (auto &&h : hash_family.get_hashes()) {
-            data.set(h % S);
+            data.set(_size.reduce2(h));
         }
     }
     bool contains_this() const {
         bool contains = true;
         for (auto &&h : hash_family.get_hashes()) {
-            contains &= data.test(h % S);
+            contains &= data.test(_size.reduce2(h));
         }
         return contains;
     }
     bool contains(const std::string &s) {
         bool contains = true;
         for (auto &&h : hash_family.hash(s)) {
-            contains &= data.test(h % S);
+            contains &= data.test(_size.reduce2(h));
         }
         return contains;
     }
 
   private:
     DynamicBitset data;
+    Modulus _size;
     H hash_family;
 };
 
--- tests/bloom_filter/benchmark_test.cpp
+++ tests/bloom_filter/benchmark_test.cpp
@@ -20,7 +20,7 @@ string rand_seq(size_t len) {
 template <class T>
 void roll_benchmark(size_t len, size_t k, const string &name) {
     cout << endl << "Benchmarking " << name << endl;
-    T bf(7, k);
+    T bf(10 * len, 7, k);
     string s = rand_seq(len + k - 1);
     unordered_set<string> strings;
     for (size_t i = 0; i < len; i++) {
@@ -73,9 +73,18 @@ void roll_benchmark(size_t len, size_t k, const string &name) {
 }
 
 template <class T>
+T create(std::size_t len, std::size_t k) {
+    if constexpr (std::is_same_v<T, std::unordered_set<std::string>>) {
+        return T();
+    } else {
+        return T(10 * len, 7);
+    }
+}
+
+template <class T>
 void benchmark(size_t len, size_t k, const string &name) {
     cout << endl << "Benchmarking " << name << endl;
-    T bf(7);
+    T bf = create<T>(10 * len, k);
     unordered_set<string> strings;
     for (size_t i = 0; i < len; i++) {
         strings.insert(rand_seq(k));
@@ -126,9 +135,9 @@ void benchmark(size_t len, size_t k, const string &name) {
 int main() {
     const size_t NUM = 1000000;
     const size_t K = 31;
-    using bf1 = BloomFilter<10 * NUM, poly_hash_family>;
-    using bf2 = BloomFilter<10 * NUM, std_hash_family>;
-    using rbf1 = RollingBloomFilter<10 * NUM, poly_hash_family>;
+    using bf1 = BloomFilter<poly_hash_family>;
+    using bf2 = BloomFilter<std_hash_family>;
+    using rbf1 = RollingBloomFilter<poly_hash_family>;
 
     benchmark<bf1>(NUM, K, "Bloom filter, rolling hash");
     benchmark<bf2>(NUM, K, "Bloom filter, std hash");
